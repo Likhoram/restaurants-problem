00fea8c90d86948b74d40d909ce6dae90fbb049a
Wenxin Li <likhoram24@gmail.com>
2025-12-09 15:14:53 -0600

    completed

--- restaurants.py
+++ restaurants.py
@@ -1,7 +1,36 @@
 def most_varied_visitor(visits):
-    # Write your solution here!
-    pass
+    #a counter to record how many different restaurants a person went to
+    #create a dictionary to store the numbers. key: name of the person. value: count of different restaurants. 
+    #loop through input dictionary, each name will be the new dict's key. the key of input dict (restaurant) will be the value of new dict (number of count).
+    count_dict = {}
+    count = 0
+    for name_list in visits.values(): 
+        #convert the list to set to get rid of repetitive name in same restaurant
+        name_set = set(name_list)
+        # print(name_set)
+        for name in name_set:
+            # if name not in count_dict:
+            #     count_dict[name] = 1
+            # else:
+            #     count_dict[name] += 1
+            count_dict[name] = count_dict.get(name, 0) + 1
+    
+    print(count_dict)
 
+    #{"Eliza": 1, "Xinting": 2}
+    #check who has highest count
+    highest_count = 0
+    highest_name = ""
+    for name, count in count_dict.items():
+        if count > highest_count:
+            highest_count = count
+            highest_name = name
+
+    print(highest_name)
+
+    return highest_name     
+
+    #return the name with highest count. 
 
 visits_1 = {
     "Spicy City" : ["Eliza"],
